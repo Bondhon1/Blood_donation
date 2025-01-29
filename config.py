4b818c20442a2009cf7120e54e92e5438dc83865
Bondhon1 <152187754+Bondhon1@users.noreply.github.com>
2025-01-29 23:13:40 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://avnadmin:AVNS_i8URZUT8EKZ0RZ0Pr9G@pg-abada35-bondhon0101-0b86.d.aivencloud.com:12523/defaultdb?sslmode=require'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+      #'postgresql://postgres:bondhon1@localhost:5432/blood_recipient_db'
