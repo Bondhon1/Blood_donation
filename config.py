10624644b75459d101b93e0ca29221f848e328c5
Bondhon1 <bondhon0101@gmail.com>
2025-01-29 23:07:18 +0600

    Remove sensitive data and clean up code

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+import os
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://avnadmin:AVNS_i8URZUT8EKZ0RZ0Pr9G@pg-abada35-bondhon0101-0b86.d.aivencloud.com:12523/defaultdb?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
       #'postgresql://postgres:bondhon1@localhost:5432/blood_recipient_db'
