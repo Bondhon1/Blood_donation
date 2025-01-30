a7f722b86137725174f8aa446d7bac215a7f1b5a
Bondhon1 <bondhon0101@gmail.com>
2025-01-30 12:43:12 +0600

    added common funtionalities of login/register. Also added little bit styling.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, session, request
+from flask import Flask, render_template, redirect, url_for, flash, session, request, jsonify
 from config import Config
 from models import db, User
 from forms import RegistrationForm, LoginForm
@@ -17,12 +17,42 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        existing_user = User.query.filter((User.username == form.username.data) | (User.email == form.email.data)).first()
+        
+        if existing_user:
+            if existing_user.username == form.username.data:
+                flash("Username is already taken.", "danger")
+            elif existing_user.email == form.email.data:
+                flash("Email is already in use.", "danger")
+            return redirect(url_for('register'))
+        
         new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(new_user)
         db.session.commit()
         flash("Registration successful! Please login.", "success")
         return redirect(url_for('login'))
+    
     return render_template('register.html', form=form)
+
+
+@app.route('/check_user', methods=['POST'])
+def check_user():
+    data = request.json
+    username = data.get("username")
+    email = data.get("email")
+
+    user_exists = User.query.filter((User.username == username) | (User.email == email)).first()
+    
+    response = {"exists": False}
+
+    if user_exists:
+        response["exists"] = True
+        if user_exists.username == username:
+            response["message"] = "Username is already taken."
+        elif user_exists.email == email:
+            response["message"] = "Email is already in use."
+
+    return jsonify(response)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
