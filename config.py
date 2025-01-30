dc30d29067c8f52ebc5158a20cf02b450b7009f3
Bondhon1 <152187754+Bondhon1@users.noreply.github.com>
2025-01-30 14:22:12 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://avnadmin:contact me for this part.d.aivencloud.com:12523/defaultdb?sslmode=require'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-      #'postgresql://postgres:bondhon1@localhost:5432/blood_recipient_db'
